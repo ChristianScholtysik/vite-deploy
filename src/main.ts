108aa4bd3b0da42a328895b4970324dd173f077e
Christian Scholtysik <christianscholtysik@gmail.com>
2024-06-24 14:14:17 +0200

    create vite test projekt

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import "./style.css";
+import * as lodash from "lodash";
+function addNumbers(number1: number, number2: number) {
+  const result = number1 + number2;
+  console.log(result);
+}
+
+console.log(addNumbers(8, 4));
+
+const superCodeKebab = lodash.kebabCase("HALLO SUPERCODE");
+console.log(superCodeKebab);
